249f1297c79cda68fb369fe1f3c7eb76a7ae2b8f
AkashEE007 <akashshetty508@gmail.com>
2025-03-15 21:14:30 +0530

    Learnt some kivy functions

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Akash S H")
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class MainWidget(Widget):
+    pass
+
+class TheLabApp(App):
+    pass
+
+TheLabApp().run()
+
+
