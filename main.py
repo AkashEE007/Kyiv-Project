b4909758751603e1c4d6a3e0df9c1fc9a17e1f00
AkashEE007 <akashshetty508@gmail.com>
2025-03-15 23:44:53 +0530

    Learnt adding the buttons and positioning on the layout

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 from kivy.uix.widget import Widget
+
+class BoxLayoutExample(BoxLayout):
+    pass
+    """def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = "vertical"
+        b1 = Button(text="Button1")
+        b2 = Button(text="Button2")
+        self.add_widget(b2)
+        self.add_widget(b1)"""
 
 
 class MainWidget(Widget):
@@ -9,5 +21,3 @@ class TheLabApp(App):
     pass
 
 TheLabApp().run()
-
-
